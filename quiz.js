8c62917cbd6710d84d72eb7bbbf6ca94c945b0e2
ph-swadeb-07 <swadebsharma2022@gmail.com>
2023-02-04 01:36:37 +0600

    adde quiz

---
+++ quiz.js
@@ -0,0 +1,28 @@
+// const nayoks= [“Bangla Bhai”, ”English Bhai”, ”Korean Bhai ”,”Turkish Bhai”];
+
+const nayoks = ['Bangla Bhai', "English Bhai", 'Korean Bhai', 'Turkish Bhai'];
+
+for(let i = 0; i <=5; i++){
+    const nayok = nayoks[i];
+    // console.log(nayok)
+}
+
+// 2
+const x = 23;
+const y = 36;
+// console.log(Math.min(x, y));
+
+//3
+let sum=0; 
+for( let i = 0; i<=3;i++){ 
+sum = sum + i; 
+// console.log(sum)
+} 
+
+// 4
+var student = { name : "Yo Mama", age : 17 };
+console.log(student)
+const deleteAge = remove(student.age);
+
+console.log(deleteAge)
+
